fcac2343f0e5272a5f07a7e9aff052ccd12a66cf
Webpress0325 <webpress021@gmail.com>
2020-12-12 17:21:22 -0800

    shop commit

---
+++ shop/admin.py
@@ -0,0 +1,8 @@
+from .models import Product, Contact, Orders, OrderUpdate
+from django.contrib import admin
+
+admin.site.register(Product)
+admin.site.register(Contact)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+
